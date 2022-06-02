cd19cab8815a4a797dce833e81caed9877c2185b
Ivan B <ivar110kg@gmail.com>
2022-06-02 21:50:31 +0400

    Add file Test2.java

---
+++ src/main/java/io/github/ivan100kg/spring_intro/Test2.java
@@ -0,0 +1,13 @@
+package io.github.ivan100kg.spring_intro;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test2 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        Pet pet = context.getBean("myPet", Pet.class);
+        pet.say();
+        context.close();
+    }
+}
