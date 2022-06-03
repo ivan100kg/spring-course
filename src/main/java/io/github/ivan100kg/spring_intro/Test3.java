027cd6992a6cf47b656268e95138f4d848439d63
Ivan B <ivar110kg@gmail.com>
2022-06-03 18:22:14 +0400

    Add new file Test3.java

---
+++ src/main/java/io/github/ivan100kg/spring_intro/Test3.java
@@ -0,0 +1,19 @@
+package io.github.ivan100kg.spring_intro;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test3 {
+    public static void main(String[] args) {
+//        Pet pet = new Cat();
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+//        Pet pet = context.getBean("myPet", Pet.class);
+//        Person person = new Person(pet);
+//        person.callYourPet();
+        Person person = context.getBean("myPerson", Person.class);
+        person.callYourPet();
+        context.close();
+    }
+}
