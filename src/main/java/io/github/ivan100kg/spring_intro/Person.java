982d4d6b7287157ad8fb2f3cc86c2765f7a217a6
Ivan B <ivar110kg@gmail.com>
2022-06-05 14:49:22 +0400

    Update Person

--- src/main/java/io/github/ivan100kg/spring_intro/Person.java
+++ src/main/java/io/github/ivan100kg/spring_intro/Person.java
@@ -2,6 +2,26 @@ package io.github.ivan100kg.spring_intro;
 
 public class Person {
     private Pet pet;
+    private String surname;
+    private int age;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        System.out.println("setSurname()");
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        System.out.println("setAge()");
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public Person(Pet pet) {
         System.out.println("Person bean is created");
