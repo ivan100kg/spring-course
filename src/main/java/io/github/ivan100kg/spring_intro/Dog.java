174890d56eb6f98c9235ff8f7ae886f39511c672
Ivan B <ivar110kg@gmail.com>
2022-06-05 16:05:01 +0400

    Update Dog.java

--- src/main/java/io/github/ivan100kg/spring_intro/Dog.java
+++ src/main/java/io/github/ivan100kg/spring_intro/Dog.java
@@ -1,6 +1,7 @@
 package io.github.ivan100kg.spring_intro;
 
 public class Dog implements Pet{
+    private String name;
     public Dog() {
         System.out.println("Dog bean is created");
 
@@ -9,5 +10,13 @@ public class Dog implements Pet{
     @Override
     public void say() {
         System.out.println("Bow-Bow");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
