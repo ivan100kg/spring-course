405a96bfb751aa42004356cba136358a87623946
Ivan B <ivar110kg@gmail.com>
2022-06-05 16:05:44 +0400

    Add Test4.java

---
+++ src/main/java/io/github/ivan100kg/spring_intro/Test4.java
@@ -0,0 +1,20 @@
+package io.github.ivan100kg.spring_intro;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test4 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext2.xml"
+        );
+        Dog dog1 = context.getBean("myPet", Dog.class);
+        Dog dog2 = context.getBean("myPet", Dog.class);
+        dog1.setName("Sharik");
+        dog2.setName("Bobik");
+
+        System.out.println(dog1 == dog2);
+        System.out.println("dog1 name: " + dog1.getName());
+        System.out.println("dog2 name: " + dog2.getName());
+        context.close();
+    }
+}
