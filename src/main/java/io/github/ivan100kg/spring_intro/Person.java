bfc912c6f168cfc7ce86d7ef3d70be03c567a231
Ivan B <ivar110kg@gmail.com>
2022-06-07 12:40:59 +0400

    Update Cat.java Person.java

--- src/main/java/io/github/ivan100kg/spring_intro/Person.java
+++ src/main/java/io/github/ivan100kg/spring_intro/Person.java
@@ -1,9 +1,20 @@
 package io.github.ivan100kg.spring_intro;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("personBean")
 public class Person {
     private Pet pet;
     private String surname;
     private int age;
+
+    @Autowired
+    public Person(Pet pet) {
+        System.out.println("Person bean is created");
+        this.pet = pet;
+    }
+
 
     public String getSurname() {
         return surname;
@@ -23,14 +34,10 @@ public class Person {
         this.age = age;
     }
 
-    public Person(Pet pet) {
-        System.out.println("Person bean is created");
-        this.pet = pet;
-    }
 
-    public Person() {
-        System.out.println("Person bean is created");
-    }
+//    public Person() {
+//        System.out.println("Person bean is created");
+//    }
 
     public void setPet(Pet pet) {
         this.pet = pet;
