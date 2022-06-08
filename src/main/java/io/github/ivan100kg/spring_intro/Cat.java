554b49731b136b2aa9838d2281c685046b2aafbc
Ivan B <ivar110kg@gmail.com>
2022-06-08 10:28:21 +0400

    Update Cat

--- src/main/java/io/github/ivan100kg/spring_intro/Cat.java
+++ src/main/java/io/github/ivan100kg/spring_intro/Cat.java
@@ -1,8 +1,10 @@
 package io.github.ivan100kg.spring_intro;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component("catBean")
+//@Component("catBean")
+//@Scope("prototype")
 public class Cat implements Pet{
     public Cat() {
         System.out.println("Cat bean is created");
