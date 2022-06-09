7a685dd2211935ed3a0267c202c2ea861663e9a6
Ivan B <ivar110kg@gmail.com>
2022-06-09 10:48:52 +0400

    Fix LoggingAspect

--- src/main/java/io/github/ivan100kg/aop/aspects/LoggingAspect.java
+++ src/main/java/io/github/ivan100kg/aop/aspects/LoggingAspect.java
@@ -2,27 +2,28 @@ package io.github.ivan100kg.aop.aspects;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
-public class LoggingAndSecurityAspect {
+@Order(10)
+public class LoggingAspect {
 
-    @Pointcut("execution(* io.github.ivan100kg.aop.UniversityLibrary.*(..))")
-    private void allMethodsFromUL(){
-    }
-
-    @Pointcut("execution(public void returnMagazine())")
-    private void returnMagazineFromUL(){}
-
-    @Pointcut("allMethodsFromUL() && !returnMagazineFromUL()")
-    private void allMethodsExceptReturnMagazineFromUL(){}
-
-    @Before("allMethodsExceptReturnMagazineFromUL()")
-    public void beforeAllMethodsExceptReturnMagazineAdvice(){
-        System.out.println("beforeAllMethodsExceptReturnMagazineAdvice: Log #4");
-    }
+//    @Pointcut("execution(* io.github.ivan100kg.aop.UniversityLibrary.*(..))")
+//    private void allMethodsFromUL(){
+//    }
+//
+//    @Pointcut("execution(public void returnMagazine())")
+//    private void returnMagazineFromUL(){}
+//
+//    @Pointcut("allMethodsFromUL() && !returnMagazineFromUL()")
+//    private void allMethodsExceptReturnMagazineFromUL(){}
+//
+//    @Before("allMethodsExceptReturnMagazineFromUL()")
+//    public void beforeAllMethodsExceptReturnMagazineAdvice(){
+//        System.out.println("beforeAllMethodsExceptReturnMagazineAdvice: Log #4");
+//    }
 
 
 //    @Pointcut("execution(void get*())")
@@ -53,19 +54,12 @@ public class LoggingAndSecurityAspect {
 //    }
 
 
-//    @Pointcut("execution(* get*())")
-//    private void allGetMethods() {
-//    }
-//
-//    @Before("allGetMethods()")
-//    public void beforeGetLoggingAdvice() {
-//        System.out.println("beforeGetLoggingAdvice: log of trying to get the paper");
-//    }
-//
-//    @Before("allGetMethods()")
-//    public void beforeGetSecurityAdvice() {
-//        System.out.println("beforeGetSecurityAdvice: check rights");
-//    }
+
+    @Before("io.github.ivan100kg.aop.aspects.MyPointcuts.allGetMethods()")
+    public void beforeGetLoggingAdvice() {
+        System.out.println("beforeGetLoggingAdvice: log of trying to get the paper");
+    }
+
 
 //    @Before("execution(* *(..))")
 //    public void beforeReturnBookAdvice() {
