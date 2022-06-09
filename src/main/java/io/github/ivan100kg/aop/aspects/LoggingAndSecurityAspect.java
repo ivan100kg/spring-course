81c150f3c81a1b32a0aa99e9bd5abaeb393bf671
Ivan B <ivar110kg@gmail.com>
2022-06-09 10:04:30 +0400

    Add pointcut combinations

--- src/main/java/io/github/ivan100kg/aop/aspects/LoggingAndSecurityAspect.java
+++ src/main/java/io/github/ivan100kg/aop/aspects/LoggingAndSecurityAspect.java
@@ -9,32 +9,48 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LoggingAndSecurityAspect {
 
-    @Pointcut("execution(void get*())")
-    private void allGetMethodsFromUL() {
+    @Pointcut("execution(* io.github.ivan100kg.aop.UniversityLibrary.*(..))")
+    private void allMethodsFromUL(){
     }
 
-    @Pointcut("execution(void return*())")
-    private void allReturnMethodsFromUL() {
+    @Pointcut("execution(public void returnMagazine())")
+    private void returnMagazineFromUL(){}
+
+    @Pointcut("allMethodsFromUL() && !returnMagazineFromUL()")
+    private void allMethodsExceptReturnMagazineFromUL(){}
+
+    @Before("allMethodsExceptReturnMagazineFromUL()")
+    public void beforeAllMethodsExceptReturnMagazineAdvice(){
+        System.out.println("beforeAllMethodsExceptReturnMagazineAdvice: Log #4");
     }
 
-    @Pointcut("execution(void get*()) || execution(void return*())")
-    private void allGetAndReturnMethodsFromUL() {
-    }
 
-    @Before("allGetMethodsFromUL()")
-    public void beforeGetLoggingAdvice() {
-        System.out.println("beforeGetLoggingAdvice: write Log #1");
-    }
-
-    @Before("allReturnMethodsFromUL()")
-    public void beforeReturnLoggingAdvice() {
-        System.out.println("beforeReturnLoggingAdvice: write Log #2");
-    }
-
-    @Before("allGetAndReturnMethodsFromUL()")
-    public void beforeGetAndReturnLoggingAdvice() {
-        System.out.println("beforeGetAndReturnLoggingAdvice: write Log #3");
-    }
+//    @Pointcut("execution(void get*())")
+//    private void allGetMethodsFromUL() {
+//    }
+//
+//    @Pointcut("execution(void return*())")
+//    private void allReturnMethodsFromUL() {
+//    }
+//
+//    @Pointcut("execution(void get*()) || execution(void return*())")
+//    private void allGetAndReturnMethodsFromUL() {
+//    }
+//
+//    @Before("allGetMethodsFromUL()")
+//    public void beforeGetLoggingAdvice() {
+//        System.out.println("beforeGetLoggingAdvice: write Log #1");
+//    }
+//
+//    @Before("allReturnMethodsFromUL()")
+//    public void beforeReturnLoggingAdvice() {
+//        System.out.println("beforeReturnLoggingAdvice: write Log #2");
+//    }
+//
+//    @Before("allGetAndReturnMethodsFromUL()")
+//    public void beforeGetAndReturnLoggingAdvice() {
+//        System.out.println("beforeGetAndReturnLoggingAdvice: write Log #3");
+//    }
 
 
 //    @Pointcut("execution(* get*())")
