a5ce9eae3130a514ba2aac7b4baefdc98379c744
Ivan B <ivar110kg@gmail.com>
2022-06-09 09:49:03 +0400

    Add new methods

--- src/main/java/io/github/ivan100kg/aop/UniversityLibrary.java
+++ src/main/java/io/github/ivan100kg/aop/UniversityLibrary.java
@@ -6,14 +6,27 @@ import org.springframework.stereotype.Component;
 public class UniversityLibrary extends AbstractLibrary {
 //    @Override
     public void getBook() {
-        System.out.println("We are getting the book from University Library");
+        System.out.println("get the book from UL");
     }
 
     public void getMagazine() {
-        System.out.println("We are getting the magazine from University Library");
+        System.out.println("get the magazine from UL");
     }
 
-//    public void returnBook() {
-//        System.out.println("We are returning the book to University Library");
-//    }
+    public void returnBook() {
+        System.out.println("returning the book to UL");
+    }
+
+    public void returnMagazine() {
+        System.out.println("return the magazine to UL");
+    }
+
+    public void addBook() {
+        System.out.println("add the book to UL");
+    }
+
+    public void addMagazine() {
+        System.out.println("add the magazine to UL");
+    }
+
 }
