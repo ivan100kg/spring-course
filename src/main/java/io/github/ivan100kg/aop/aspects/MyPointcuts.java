c617b499316cb9c767bed685efb4dec7fbb7e9e8
Ivan B <ivar110kg@gmail.com>
2022-06-09 10:48:12 +0400

    Add file MyPointcuts.java

---
+++ src/main/java/io/github/ivan100kg/aop/aspects/MyPointcuts.java
@@ -0,0 +1,10 @@
+package io.github.ivan100kg.aop.aspects;
+
+import org.aspectj.lang.annotation.Pointcut;
+
+public class MyPointcuts {
+
+    @Pointcut("execution(* get*())")
+    public void allGetMethods() {
+    }
+}
