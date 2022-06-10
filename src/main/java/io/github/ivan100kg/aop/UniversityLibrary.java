ca379ed95af6ef97e56f66ac8db0c5576df6930b
Ivan B <ivar110kg@gmail.com>
2022-06-10 10:05:40 +0400

    Update UniversityLibrary

--- src/main/java/io/github/ivan100kg/aop/UniversityLibrary.java
+++ src/main/java/io/github/ivan100kg/aop/UniversityLibrary.java
@@ -7,26 +7,37 @@ public class UniversityLibrary extends AbstractLibrary {
 //    @Override
     public void getBook() {
         System.out.println("get the book from UL");
+        System.out.println("-------------------------");
     }
 
     public void getMagazine() {
         System.out.println("get the magazine from UL");
+        System.out.println("-------------------------");
+
     }
 
     public void returnBook() {
         System.out.println("returning the book to UL");
+        System.out.println("-------------------------");
+
     }
 
     public void returnMagazine() {
         System.out.println("return the magazine to UL");
+        System.out.println("-------------------------");
+
     }
 
-    public void addBook() {
-        System.out.println("add the book to UL");
+    public void addBook(String personName, Book book) {
+        System.out.println("add the book: '" + book.getName() + "' to UL");
+        System.out.println("-------------------------");
+
     }
 
     public void addMagazine() {
         System.out.println("add the magazine to UL");
+        System.out.println("-------------------------");
+
     }
 
 }
