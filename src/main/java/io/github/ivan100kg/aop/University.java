a7733b2280c0e1428271a4c167e77931014cf909
Ivan B <ivar110kg@gmail.com>
2022-06-10 10:05:30 +0400

    Update University

--- src/main/java/io/github/ivan100kg/aop/University.java
+++ src/main/java/io/github/ivan100kg/aop/University.java
@@ -20,6 +20,7 @@ public class University {
 
     public List<Student> getStudents() {
         System.out.println("getStudents starts");
+        if (false) throw new IllegalStateException("My Test Exception");
         System.out.println("Information from method getStudents:");
         System.out.println(students);
         return students;
