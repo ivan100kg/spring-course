f1bdd877476175681637d3d8ebb90fc6141be3ce
Ivan B <ivar110kg@gmail.com>
2022-06-14 11:01:46 +0400

    Add file Detail.java

---
+++ src/main/java/io/github/ivan100kg/hibernate2/entity/Detail.java
@@ -0,0 +1,70 @@
+package io.github.ivan100kg.hibernate2.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "details")
+public class Detail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private int id;
+    @Column
+    private String city;
+    @Column
+    private String phone_number;
+    @Column
+    private  String email;
+
+    public Detail() {
+    }
+
+    public Detail(String city, String phone_number, String email) {
+        this.city = city;
+        this.phone_number = phone_number;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Detail{" +
+                "id=" + id +
+                ", city='" + city + '\'' +
+                ", phone_number='" + phone_number + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
