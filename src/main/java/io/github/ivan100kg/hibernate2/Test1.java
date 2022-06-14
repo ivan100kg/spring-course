b897aa74a9704d7daa2e925a5113864541147081
Ivan B <ivar110kg@gmail.com>
2022-06-14 11:20:59 +0400

    Add One-to-One INSERT

--- src/main/java/io/github/ivan100kg/hibernate2/Test1.java
+++ src/main/java/io/github/ivan100kg/hibernate2/Test1.java
@@ -1,6 +1,7 @@
 package io.github.ivan100kg.hibernate2;
 
-import io.github.ivan100kg.hibernate_learning.entity.Employee;
+import io.github.ivan100kg.hibernate2.entity.Detail;
+import io.github.ivan100kg.hibernate2.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -10,10 +11,14 @@ public class Test1 {
         try(SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
                 .buildSessionFactory()) {
+            Employee emp = new Employee("Oleg", "Pacman", "Security", 230);
+            Detail detail = new Detail("Samara", "+79172309076", "oleg@gmail.com");
+            emp.setEmpDetail(detail);
             Session session = factory.getCurrentSession();
             session.beginTransaction();
-
+            session.save(emp);
 
 
             session.getTransaction().commit();
